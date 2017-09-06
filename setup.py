097278bcd06c0c03eab74f9dd9b99563da1f4019
Derek Santos <derekhalo@gmail.com>
2017-09-06 19:27:53 -0400

    Cleaned setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='Direct Downloader',
-      version='0.2',
+      version='0.3',
       description='Download a list of direct links using multi-threading.',
       author='Derek Santos',
       license='The MIT License (MIT)',
-      url='https://github.com/santosderek/dDownloader',
+      url='https://github.com/santosderek/Direct-Downloader',
       packages=['direct_downloader'],
       scripts=['direct_downloader/main.py',
                'direct_downloader/direct_downloader.py'],
@@ -17,5 +17,6 @@ setup(name='Direct Downloader',
       install_requires=[
           'requests',
           'bs4'
-      ]
+      ],
+      keywords=['testing', 'logging', 'example']
       )
